bc0465b7b610a8f497cff5b527b20d9d02f77f54
Trey Davis <treydavis@SoundMachine-MBP.local>
2023-09-18 22:00:06 -0400

    Now finish cs121driver2.cpp

---
+++ cs121driver1.cpp
@@ -0,0 +1,25 @@
+/*****************************************************************************************
+ ** File name       : CS121_Lab1
+ **
+ ** This program prints 'Waz up ? \n'
+ **
+ **
+ **
+ ** Programmer       : Trey Davis
+ **
+ ** Date created     : 9/18/2023
+ **
+ ** Date last revised :
+ **
+ **
+ **
+ **
+*****************************************************************************************/
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int main() {
+    cout << "Waz up ? \n" << std::endl;
+    return 0;
+}
