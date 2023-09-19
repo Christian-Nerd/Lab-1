d4a425fc4ad95692690366c4d03d0ded23618343
Trey Davis <treydavis@SoundMachine-MBP.local>
2023-09-18 23:48:01 -0400

    Commit #1 message was a mistake, but finished CS121_Lab1 and must test everything on windows

--- cs121driver1.cpp
+++ cs121driver1.cpp
@@ -1,25 +1,47 @@
-/*****************************************************************************************
- ** File name       : CS121_Lab1
- **
- ** This program prints 'Waz up ? \n'
- **
- **
- **
- ** Programmer       : Trey Davis
- **
- ** Date created     : 9/18/2023
- **
- ** Date last revised :
- **
- **
- **
- **
-*****************************************************************************************/
+    /*****************************************************************************************
+     ** File name       : CS121_Lab1
+     **
+     ** This program prints 'Waz up ? \n'
+     **
+     **
+     **
+     ** Programmer       : Trey Davis
+     **
+     ** Date created     : 9/18/2023
+     **
+     ** Date last revised :
+     **
+     **
+     **
+     **
+    *****************************************************************************************/
 #include <iostream>
-#include <climits>
+
 using namespace std;
 
-int main() {
+int main()
+{
     cout << "Waz up ? \n" << std::endl;
+    cout << "We are computing bits and bytes of the primitive data types \n ";
+    cout << endl <<endl;
+
+    cout << "Number of bytes in a short integer = " << sizeof(short) << "; number of bits" << sizeof(short) * 8 << "\n\n";
+
+    cout << "Number of bytes in an unsigned short integer = " << sizeof(unsigned short) << "; number of bits = " << sizeof(unsigned short) * 8 << "\n\n";
+
+    cout << "Number of bytes in an integer = " << sizeof(int) << "; number of bits = " << sizeof(int) * 8 << "\n\n";
+
+    cout << "Number of bytes in an unsigned integer = " << sizeof(unsigned int) << "; number of bits = " << sizeof(unsigned int) * 8 << "\n\n";
+
+    cout << "Number of bytes in an long integer = " << sizeof(long) << "; number of bits = " << sizeof(long) * 8 << "\n\n";
+
+    cout << "Number of bytes in an unsigned long = " << sizeof(unsigned long) << "; number of bits = " << sizeof(unsigned long) * 8 << "\n\n";
+
+    cout << "Number of bytes in an character = " << sizeof(char) << "; number of bits = " << sizeof(char) * 8 << "\n\n";
+
+    cout << "Number of bytes in an float = " << sizeof(float) << "; number of bits = " << sizeof(float) * 8 << "\n\n";
+
+    cout << "Number of bytes in an double = " << sizeof(double) << "; number of bits = " << sizeof(double) * 8 << "\n\n";
+
     return 0;
 }
---
+++ cs121driver2.cpp
@@ -0,0 +1,23 @@
+/*****************************************************************************************
+ ** File name       : CS121_Lab1
+ **
+ ** This program prints 'Waz up ? \n'
+ **
+ **
+ **
+ ** Programmer       : Trey Davis
+ **
+ ** Date created     : 9/18/2023
+ **
+ ** Date last revised :
+ **
+ **
+ **
+ **
+*****************************************************************************************/
+
+#include <iostream>
+#include <climits>
+#include <cfloat>
+
+using namespace std;
